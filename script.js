620c71f71777a417fe856dbd555fde1badb5d905
PasterIvan <paster-ivan@yandex.by>
2023-04-30 23:10:28 +0300

    feat: add keys

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const Keyboard = {
+const Body = {
   elements: {
     main: document.createElement("main"),
     text: document.createElement("textarea"),
@@ -15,11 +15,36 @@ const Keyboard = {
     this.elements.main.appendChild(this.elements.text);
     this.elements.main.appendChild(this.elements.keyboard);
 
+    this.elements.keyboard.appendChild(this.createKeys());
+
     document.body.appendChild(this.elements.main);
   },
 
+  createKeys() {
+    const fragment = document.createDocumentFragment();
+    const keys = [
+      "§", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "backspace",
+      "tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]","enter",
+      "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "|", "r-shift",
+      "l-shift", "`", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "up",
+      "fn", "ctr", "option", "command", "space", "command", "option", "left", "down", "right"
+    ];
+
+    // Creates HTML for an icon
+    keys.forEach(key => {
+      const button = document.createElement("button");
+
+      // Add attributes/classes
+      button.setAttribute("type", "button");
+      button.classList.add("button");
+      button.textContent = key.toLowerCase();
+
+      fragment.appendChild(button);
+
+    });
+
+    return fragment;
+  },
 };
 
-window.addEventListener("DOMContentLoaded", function () {
-  Keyboard.init();
-});
+window.addEventListener("DOMContentLoaded", () => Body.init());
