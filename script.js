9643627ba54af575643681d6b52103644eb033c1
PasterIvan <paster-ivan@yandex.by>
2023-05-01 00:15:56 +0300

    feat: add animation buttons

--- script.js
+++ script.js
@@ -34,8 +34,8 @@ const Body = {
     const fragment = document.createDocumentFragment();
     const keys = [
       "§", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace",
-      "tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]","Enter",
-      "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "|", "r-shift",
+      "Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]","Enter",
+      "Caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "|", "r-shift",
       "l-shift", "`", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "up",
       "fn", "ctr", "option", "command", "Space", "command", "option", "left", "down", "right"
     ];
@@ -52,12 +52,26 @@ const Body = {
       switch (key) {
         case "Backspace":
           button.classList.add("button_backspace");
+
           button.addEventListener("click", () => {
             this.value = this.value.slice(0, -1);
             document.querySelectorAll(".text").forEach(element => {
               element.value = this.value;
             });
           });
+
+          document.addEventListener('keydown', (event) => {
+            if (event.code === "Backspace") {
+              button.classList.add("backspace-active");
+            }
+          });
+
+          document.addEventListener('keyup', (event) => {
+            if (event.code === "Backspace") {
+              button.classList.remove("backspace-active");
+            }
+          });
+
           break;
 
         case "Enter":
@@ -66,29 +80,124 @@ const Body = {
               this.value += "\n";
             });
           });
+
+          document.addEventListener('keydown', (event) => {
+            if (event.code === "Enter") {
+              button.classList.add("active");
+            }
+          });
+
+          document.addEventListener('keyup', (event) => {
+            if (event.code === "Enter") {
+              button.classList.remove("active");
+            }
+          });
+
           break;
 
-        case "tab":
+        case "Tab":
           button.classList.add("button_tab");
 
+          document.addEventListener('keydown', (event) => {
+            if (event.code === "Tab") {
+              button.classList.add("tab-active");
+            }
+          });
+
+          document.addEventListener('keyup', (event) => {
+            if (event.code === "Tab") {
+              button.classList.remove("tab-active");
+            }
+          });
+
           break;
+
         case "r-shift":
           button.classList.add("button_r-shift");
 
+          document.addEventListener('keydown', (event) => {
+            if (event.code === "ShiftRight") {
+              button.classList.add("r-shift-active");
+            }
+          });
+
+          document.addEventListener('keyup', (event) => {
+            if (event.code === "ShiftRight") {
+              button.classList.remove("r-shift-active");
+            }
+          });
+
           break;
+
         case "l-shift":
           button.classList.add("button_r-shift");
 
+          document.addEventListener('keydown', (event) => {
+            if (event.code === "ShiftLeft") {
+              button.classList.add("l-shift-active");
+            }
+          });
+
+          document.addEventListener('keyup', (event) => {
+            if (event.code === "ShiftLeft") {
+              button.classList.remove("l-shift-active");
+            }
+          });
+
           break;
+
+          case "Caps":
+          button.classList.add("button_cups");
+
+          document.addEventListener('keydown', (event) => {
+            if (event.code === "CapsLock") {
+              button.classList.add("active");
+            }
+          });
+
+          document.addEventListener('keyup', (event) => {
+            if (event.code === "CapsLock") {
+              button.classList.remove("active");
+            }
+          });
+
+          break;
+
         case "up":
           button.classList.add("button_up");
 
+          document.addEventListener('keydown', (event) => {
+            if (event.code === "ArrowUp") {
+              button.classList.add("up-active");
+            }
+          });
+
+          document.addEventListener('keyup', (event) => {
+            if (event.code === "ArrowUp") {
+              button.classList.remove("up-active");
+            }
+          });
+
           break;
+
         case "Space":
           button.classList.add("button_space");
           button.addEventListener("click", () => {
             this.value += " ";
           });
+
+          document.addEventListener('keydown', (event) => {
+            if (event.code === "Space") {
+              button.classList.add("spase-active");
+            }
+          });
+
+          document.addEventListener('keyup', (event) => {
+            if (event.code === "Space") {
+              button.classList.remove("spase-active");
+            }
+          });
+
           break;
 
         case "command":
@@ -104,6 +213,18 @@ const Body = {
             document.querySelectorAll(".text").forEach(element => {
               element.value = this.value;
             });
+          });
+
+          document.addEventListener('keydown', (event) => {
+            if (event.key === key) {
+              button.classList.add("active");
+            }
+          });
+
+          document.addEventListener('keyup', (event) => {
+            if (event.key === key) {
+              button.classList.remove("active");
+            }
           });
 
           break;
