9fbdb12a3d743671dd3d030f4c6649a509796e1d
PasterIvan <paster-ivan@yandex.by>
2023-04-30 23:22:17 +0300

    fix: style keys

--- script.js
+++ script.js
@@ -23,11 +23,11 @@ const Body = {
   createKeys() {
     const fragment = document.createDocumentFragment();
     const keys = [
-      "§", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "backspace",
-      "tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]","enter",
+      "§", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace",
+      "tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]","Enter",
       "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "|", "r-shift",
       "l-shift", "`", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "up",
-      "fn", "ctr", "option", "command", "space", "command", "option", "left", "down", "right"
+      "fn", "ctr", "option", "command", "Space", "command", "option", "left", "down", "right"
     ];
 
     // Creates HTML for an icon
@@ -37,10 +37,47 @@ const Body = {
       // Add attributes/classes
       button.setAttribute("type", "button");
       button.classList.add("button");
+
+
+      switch (key) {
+        case "Backspace":
+          button.classList.add("button_backspace");
+
+          break;
+        case "tab":
+          button.classList.add("button_tab");
+
+          break;
+        case "r-shift":
+          button.classList.add("button_r-shift");
+
+          break;
+        case "l-shift":
+          button.classList.add("button_r-shift");
+
+          break;
+        case "up":
+          button.classList.add("button_up");
+
+          break;
+        case "Space":
+          button.classList.add("button_space");
+
+          break;
+        case "command":
+          button.classList.add("button_command");
+
+          break;
+
+        default:
+          button.classList.add("button");
+
+
+          break;
+      }
+
       button.textContent = key.toLowerCase();
-
       fragment.appendChild(button);
-
     });
 
     return fragment;
